a467055eeb38898483857ad9aabbb38297b7c19d
Klaus Helenius <klaus.helenius@gmail.com>
2025-02-19 16:26:30 +0200

    zshrc: show kubecontext on command

--- dotfiles/.p10k.zsh
+++ dotfiles/.p10k.zsh
@@ -165,7 +165,8 @@
 
   ##########[ kubecontext: current kubernetes context ]##################################
 
-  # typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='kubectl|helm|kubens|kubectx|oc|istioctl|kogito|k9s|helmfile|flux|fluxctl|stern|kubeseal|skaffold|kubent|kubecolor|cmctl|sparkctl'
+  typeset -g POWERLEVEL9K_KUBECONTEXT_COMMANDS='kubectl|helm|kubens|kubectx|oc|istioctl|kogito|k9s|helmfile|flux|fluxctl|stern|kubeseal|skaffold|kubent|kubecolor|cmctl|sparkctl'
+  typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND="${POWERLEVEL9K_KUBECONTEXT_COMMANDS}"
 
   ##########[ context: user@hostname ]###################################################
 
